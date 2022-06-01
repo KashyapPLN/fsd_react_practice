8e4d6d1537f39d44b3a5a74daf769d8c08175397
Kashyap PLN <kashyap.pln@gmail.com>
2022-06-01 20:14:01 +0530

    1st react practice

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function App() {
+  const names=["abc","def","ghi"];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <img src="https://www.pngitem.com/pimgs/m/146-1468479_my-profile-icon-blank-profile-picture-circle-hd.png"></img>
+     
+
+    {names.map((nm)=>
+      <Message name={nm}/>)}
     </div>
   );
 }
+function Message(props){
+  
+  return <h1>Hello, {props.name}</h1>
+  
+}
 
-export default App;
+// export default App;
